30f37ad7e2d23e3f003014de86767505926e1bfc
ps-aux <seriouz.accz@gmail.com>
2017-01-08 17:13:03 +0100

    Add some week 4 sollutions

--- assignment.sbt
+++ assignment.sbt
@@ -1,2 +1,2 @@
 course := "progfun1"
-assignment := "objsets"
+assignment := "patmat"
---
+++ src/test/scala/patmat/HuffmanSuite.scala
@@ -0,0 +1,72 @@
+package patmat
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import patmat.Huffman._
+
+@RunWith(classOf[JUnitRunner])
+class HuffmanSuite extends FunSuite {
+
+  trait TestTrees {
+    val t1 = Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5)
+    val t2 = Fork(Fork(Leaf('a', 2), Leaf('b', 3), List('a', 'b'), 5), Leaf('d', 4), List('a', 'b', 'd'), 9)
+
+    val timesL = List('a', 'b', 'c', 'a', 'a', 'b')
+  }
+
+
+  test("weight of a larger tree") {
+    new TestTrees {
+      assert(weight(t1) === 5)
+    }
+  }
+
+
+  test("chars of a larger tree") {
+    new TestTrees {
+      assert(chars(t2) === List('a', 'b', 'd'))
+    }
+  }
+
+  test("char frequency of tree") {
+
+    def find(ch: Char, l: List[(Char, Int)]): Int = l match {
+      case Nil => -1
+      case (t1, t2) :: xs => if (t1 == ch) t2 else find(ch, xs)
+    }
+
+    new TestTrees {
+      val res = times(timesL)
+      assert(find('a',res) === 3)
+      assert(find('b',res) === 2)
+      assert(find('c',res) === 1)
+    }
+  }
+
+
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
+
+  test("makeOrderedLeafList for some frequency table") {
+    assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 3)))
+  }
+
+
+  test("combine of some leaf list") {
+    val leaflist = List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4))
+    assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
+  }
+
+
+  test("decode and encode a very short text should be identity") {
+    new TestTrees {
+      assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
+    }
+  }
+
+}
